e7f3ecc18e3c224155c354d3e21bcce4b28a8a8e
KeaganGilmore <keagangilmore@gmail.com>
2024-03-22 22:23:45 +0200

    Add simple interest calculator Python script

---
+++ demo.py
@@ -0,0 +1,10 @@
+# Accepting input from the user and converting to integers
+P = int(input("Enter value for P: "))
+n = int(input("Enter value for n: "))
+R = int(input("Enter value for R: "))
+
+# Calculating simple interest
+si = (P * n * R) / 100
+
+# Displaying the result
+print("The simple interest is:", si)
